e9af8004b3f55378fd9f29db5ea1a037209034bf
Saksham Bhatia <iamsakshambhatia@gmail.com>
2025-02-04 18:32:18 +0530

    First Commit - Added all the files on git

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        spline: ['Spline Sans','sans-serif'],
+        zin: ['Zin Display Demo','sans-serif'],
+        inter: ['Inter','sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
